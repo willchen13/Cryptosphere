33dad9cd932e22896aa8e118630cb1f79f57e9e6
Will Chen <willchen@Wills-iMac.local>
2021-11-11 12:52:20 -0800

    rendered static home page

--- src/App.js
+++ src/App.js
@@ -16,16 +16,26 @@ const App = () => {
                 <Layout>
                     <div className = "routes">
                         <Routes>
-                            <Route exact path = "/" element={<HomePage/>}/>
-                            <Route exact path = "/exchanges" element={<Exchanges/>}/>
-                            <Route exact path = "/cryptocurrencies" element={<Cryptocurrencies/>}/>
-                            <Route exact path = "/crypto/:coinId" element={<CryptoDetails/>}/>
+                            <Route path = "/" element={<HomePage/>}/>
+                            <Route path = "/exchanges" element={<Exchanges/>}/>
+                            <Route path = "/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                            <Route path = "/crypto/:coinId" element={<CryptoDetails/>}/>
                         </Routes>
                     </div>
                 </Layout>
-            </div>
+            
 
             <div className ="footer">
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Cryptosphere <br/>
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to ="/news">News</Link>
+                </Space>
+            </div>
 
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import App from './App'
+import 'antd/dist/antd.css';
 
 ReactDOM.render(<Router><App/></Router>, document.getElementById('root'))
