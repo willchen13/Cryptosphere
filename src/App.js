530ce0ee32c694cfd6f65778c7292f0ae6bf565d
Will Chen <willchen@Wills-iMac.local>
2021-11-10 19:08:20 -0800

    created routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import {NavBar} from './components'
+import {NavBar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components'
 
 import './App.css';
 
@@ -13,7 +13,16 @@ const App = () => {
             </div>
     
             <div className = "main"> 
-
+                <Layout>
+                    <div className = "routes">
+                        <Routes>
+                            <Route exact path = "/" element={<HomePage/>}/>
+                            <Route exact path = "/exchanges" element={<Exchanges/>}/>
+                            <Route exact path = "/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                            <Route exact path = "/crypto/:coinId" element={<CryptoDetails/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
             </div>
 
             <div className ="footer">
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export {default as NavBar} from './NavBar.jsx';
+export {default as HomePage} from './HomePage.jsx';
+export {default as Exchanges} from './Exchanges.jsx';
+export {default as Cryptocurrencies} from './Cryptocurrencies.jsx';
+export {default as CryptoDetails} from './CryptoDetails.jsx';
+export {default as News} from './News.jsx';
